0a375c55c8d9d860e0f8f402f1c2805a638e949b
ralitsavoronevska <ralicavoronevska@gmail.com>
2024-11-12 23:26:37 +0200

    MongoDB & Mongoose

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
+
+mongoose.connect(
+  "mongodb+srv://ralitsavoronevska:" +
+    process.env.MONGO_ATLAS_PW +
+    "@node-rest-shop.e8zkr.mongodb.net/"
+);
 
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));

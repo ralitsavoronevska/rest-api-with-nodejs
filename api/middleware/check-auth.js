bd8331fdf83973325045103422bcff6b382f07f0
ralitsavoronevska <ralicavoronevska@gmail.com>
2025-01-07 18:15:48 +0200

    Adding Controllers

--- api/middleware/check-auth.js
+++ api/middleware/check-auth.js
@@ -1,36 +1,37 @@
 // https://dev.to/aneeqakhan/how-to-protect-routes-with-jwt-token-in-nodejs-fgk
 
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const asyncHandler = require("express-async-handler");
-const User = require('../models/user');
 
-const protect = asyncHandler(async (req, res, next) => {
-    let token;
-    if (
-      req.headers.authorization &&
-      req.headers.authorization.startsWith("Bearer")
-    ) {
-      try {
-        // get token from header
-        token = req.headers.authorization.split(" ")[1];
-  
-        // verify token
-        const decoded = jwt.verify(token, `${process.env.JWT_SECRET}`);
-  
-        // get user from the token
-        req.user = await User.findById(decoded.id).select("-password");
-  
-        next();
-      } catch (error) {
-        console.log(error);
-        res.status(401);
-        throw new Error("Not Authorized");
-      }
-    }
-    if (!token) {
+const User = require("../models/user");
+
+const checkAuth = asyncHandler(async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      // get token from header
+      token = req.headers.authorization.split(" ")[1];
+
+      // verify token
+      const decoded = jwt.verify(token, `${process.env.JWT_SECRET}`);
+
+      // get user from the token
+      req.user = await User.findById(decoded.id).select("-password");
+
+      next();
+    } catch (error) {
+      console.log(error);
       res.status(401);
-      throw new Error("Not Authorized, no token");
+      throw new Error("Not Authorized");
     }
-  });
-  
-  module.exports = protect;
+  }
+  if (!token) {
+    res.status(401);
+    throw new Error("Not Authorized, no token");
+  }
+});
+
+module.exports = checkAuth;
--- api/routes/products.js
+++ api/routes/products.js
@@ -1,174 +1,57 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose');
-const multer = require('multer');
-const protect = require('../middleware/check-auth')
+const multer = require("multer");
+
+const checkAuth = require("../middleware/check-auth");
+const ProductsController = require("../controllers/products");
 
 const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-    cb(null, 'uploads');
+  destination: function (req, file, cb) {
+    cb(null, "uploads");
   },
-  filename: function(req, file, cb) {
-    cb(null, new Date().toISOString().slice(0,10) + '-' + file.originalname);
-  }
-}); 
+  filename: function (req, file, cb) {
+    cb(null, new Date().toISOString().slice(0, 10) + "-" + file.originalname);
+  },
+});
 
 const fileFilter = (req, file, cb) => {
   // reject a file
-  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
     cb(null, true);
   } else {
     cb(null, false);
   }
-}
+};
 
 const upload = multer({
-  storage: storage, 
+  storage: storage,
   limits: {
-    fileSize: 1024 * 1024 * 5
+    fileSize: 1024 * 1024 * 5,
   },
-  fileFilter: fileFilter 
+  fileFilter: fileFilter,
 });
 
-const Product = require('../models/product');
+router.get("/", ProductsController.products_get_all);
 
-router.get("/", (req, res, next) => {
-  Product.find()
-    .select("name price _id productImage")
-    .exec()
-    .then((docs) => {
-      const response = {
-        count: docs.length,
-        products: docs.map((doc) => {
-          return {
-            name: doc.name,
-            price: doc.price,
-            productImage: doc.productImage,
-            _id: doc._id,
-            request: {
-              type: "GET",
-              url: "http://localhost:3000/products/" + doc._id,
-            },
-          };
-        }),
-      };
-      //   if (docs.length >= 0) {
-      res.status(200).json(response);
-      //   } else {
-      //     res.status(404).json({
-      //       message: "No entries found",
-      //     });
-      //   }
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({ error: err });
-    });
-});
+router.post(
+  "/",
+  checkAuth,
+  upload.single("productImage"),
+  ProductsController.products_create_product
+);
 
-router.post("/", protect, upload.single('productImage'), (req, res, next) => {
-  console.log(req.file);
-  const product = new Product({
-    _id: new mongoose.Types.ObjectId(),
-    name: req.body.name,
-    price: req.body.price,
-    productImage: req.file.path
-  });
-  product
-    .save()
-    .then((result) => {
-      console.log(result);
-      res.status(201).json({
-        message: "Created product successfully",
-        createdProduct: {
-          name: result.name,
-          price: result.price,
-          _id: result._id,
-          request: {
-            type: "GET",
-            url: "http://localhost:3000/products/" + result._id,
-          },
-        },
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({ error: err });
-    });
-});
+router.get("/:productId", ProductsController.products_get_product);
 
-router.get("/:productId", (req, res, next) => {
-  const id = req.params.productId;
-  Product.findById(id)
-    .select("name price _id productImage")
-    .exec()
-    .then((doc) => {
-      console.log("From database", doc);
-      if (doc) {
-        res.status(200).json({
-          product: doc,
-          request: {
-            type: "GET",
-            description: "Get all products",
-            url: "http://localhost:3000/products/",
-          },
-        });
-      } else {
-        res.status(404).json({
-          message: "No valid entry found for provided ID",
-        });
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({ error: err });
-    });
-});
+router.patch(
+  "/:productId",
+  checkAuth,
+  ProductsController.products_update_product
+);
 
-router.patch("/:productId", protect, (req, res, next) => {
-  const id = req.params.productId;
-  const updateOps = {};
-  for (const ops of req.body) {
-    updateOps[ops.propName] = ops.value;
-  }
-  Product.updateOne({ _id: id }, { $set: updateOps })
-    .exec()
-    .then((result) => {
-      res.status(200).json({
-        message: "Product updated",
-        request: {
-          type: "GET",
-          url: "http://localhost:3000/products/" + id,
-        },
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({ error: err });
-    });
-});
-
-router.delete("/:productId", protect, (req, res, next) => {
-  const id = req.params.productId;
-  Product.deleteOne({ _id: id })
-    .exec()
-    .then((result) => {
-      res.status(200).json({
-        message: "Product deleted",
-        request: {
-          type: "POST",
-          url: "http://localhost:3000/products/",
-          body: {
-            name: "String",
-            price: "Number",
-          },
-        },
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({ error: err });
-    });
-});
+router.delete(
+  "/:productId",
+  checkAuth,
+  ProductsController.products_delete_product
+);
 
 module.exports = router;
